b1c22566394a66eec9022439c04ae8aede89086f
Robert Bristow <robertbristow3@gmail.com>
2021-12-06 14:16:21 +0000

    move files to correct places

--- src/Components/BoardSquare/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function BoardSquare({id, imageSource}) {
-    return (
-            <input type='image' alt='board piece' src={imageSource} disabled className='board-btn' id={id}></input>
-    )
-}
