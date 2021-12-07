257541c6f7bee04a30ba006972a9f3b31d28dfca
Robert Bristow <robertbristow3@gmail.com>
2021-12-07 16:19:46 +0000

    add logo

--- chippers/src/Pages/ChippersPage/index.jsx
+++ chippers/src/Pages/ChippersPage/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./style.css";
 
-import { Gameboard } from "../../../../src/Components";
+import { Gameboard } from "../../Components";
 
 export function ChippersPage() {
     return (
