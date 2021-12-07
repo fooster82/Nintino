c8734cc64ad0a9c4167ff552dbde53f08f2017f1
hxckey <george.h@live.com>
2021-12-07 11:47:06 +0000

    test new users import urls.py nintino

--- Nintino/urls.py
+++ Nintino/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-# from users import views as usersViews
+import users
 from django.contrib.auth import views as auth_views
 from chippers import views as chippersViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('signup/', name='signup'),
+    path('signup/', users.views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('games/',include('chippers.urls')),
