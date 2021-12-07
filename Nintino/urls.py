5009ef15b06268ba9a795aace25538dd04913876
hxckey <george.h@live.com>
2021-12-07 11:38:16 +0000

    test remove signup views

--- Nintino/urls.py
+++ Nintino/urls.py
@@ -7,7 +7,7 @@ from chippers import views as chippersViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', name='signup'),
+    # path('signup/', name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('games/',include('chippers.urls')),
