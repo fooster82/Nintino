f9a38e2897f15ab788c038323713cb2b5875948b
hxckey <george.h@live.com>
2021-12-07 11:52:15 +0000

    test rename users folder

--- Nintino/urls.py
+++ Nintino/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-import users
+from nint_users import views
 from django.contrib.auth import views as auth_views
 from chippers import views as chippersViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', users.views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('games/',include('chippers.urls')),
