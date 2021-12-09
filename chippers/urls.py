3eae99d2064ff0306a51e399130137681da946df
hxckey <george.h@live.com>
2021-12-09 17:45:18 +0000

    fix django routing

--- chippers/urls.py
+++ chippers/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.index, name='games-index'),
     path('chippers', views.index, name='chippers-page'),
     path('lobby', views.index, name='lobby-page'),
-    path('winnersPage', views.index, name='winners-page'),
+    path('winningPage', views.index, name='winners-page'),
     path('commingsoon', views.index, name='commingsoon-page'),
     path("logout", auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 
